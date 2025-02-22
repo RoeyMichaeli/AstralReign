dfd77b8d49015301fc344a54fe3951c4b6ae0d61
RoeyMichaeli <michaeliroey@gmail.com>
2025-02-22 23:41:24 +0200

    Explicit casting. Formatting.

--- main.cpp
+++ main.cpp
@@ -10,12 +10,10 @@
 //using Eigen::Vector2f;
 using sf::Vector2f;
 
-
-
 // Components
 struct CTransform {
-	Vector2f position = { 0,0 };
-	Vector2f velocity = { 0,0 };
+	Vector2f position = { 0, 0 };
+	Vector2f velocity = { 0, 0 };
 	float angle = 0;
 
 	CTransform() = default;
@@ -79,23 +77,36 @@ struct TBullet {};
 struct TPendingDestruction {};
 
 // Configurations
-// Shape Radius		SR
+// Shape Radius   SR
 // Collision Radius CR
-// Fill Color		FR, FG, FB
-// Outline Color	OR, OG, OB
+// Fill Color     FR, FG, FB
+// Outline Color  OR, OG, OB
 // Outline Thickness OT
 // Shape Verticies  V
 // Min / Max Verticies  VMIN, VMAX
-// Speed			S
+// Speed           S
 // Min / Max Speed  SMIN, SMAX
 // Lifespan L
 // Spawn Interval   SI
 
-struct PlayerConfig { short SR, CR, OR, OG, OB, FR, FG, FB, OT, V; float S; };
-struct EnemyConfig  { short SR, CR, OR, OG, OB,				OT, VMIN, VMAX, L, SI; float SMIN, SMAX; };
-struct BulletConfig { short SR, CR, OR, OG, OB, FR, FG, FB, OT, V; float S; };
+struct PlayerConfig {
+	short SR, CR, OR, OG, OB, FR, FG, FB, OT, V;
+	float S;
+};
 
-struct EnemySpawnArea { short minX, minY, maxX, maxY; };
+struct EnemyConfig {
+	short SR, CR, OR, OG, OB, OT, VMIN, VMAX, L, SI;
+	float SMIN, SMAX;
+};
+
+struct BulletConfig {
+	short SR, CR, OR, OG, OB, FR, FG, FB, OT, V;
+	float S;
+};
+
+struct EnemySpawnArea {
+	short minX, minY, maxX, maxY;
+};
 
 class Game {
 	sf::RenderWindow m_window;
@@ -123,7 +134,7 @@ class Game {
 		m_window.create(sf::VideoMode::getDesktopMode(), "AstralReign", sf::State::Fullscreen);
 
 		// Set window properties
-		m_window.setFramerateLimit(60); // 33ms per frame
+		m_window.setFramerateLimit(60); // 33ms per frame (now 60 FPS)
 		m_window.setVerticalSyncEnabled(true);
 
 		m_screenSize = Vector2f(m_window.getSize());
@@ -140,42 +151,43 @@ class Game {
 		}
 
 		// Read player config
-		file >> m_playerConfig.SR >> m_playerConfig.CR >> 
-			m_playerConfig.FR >> m_playerConfig.FG >> m_playerConfig.FB >> 
-			m_playerConfig.OR >> m_playerConfig.OG >> m_playerConfig.OB >> 
+		file >> m_playerConfig.SR >> m_playerConfig.CR >>
+			m_playerConfig.FR >> m_playerConfig.FG >> m_playerConfig.FB >>
+			m_playerConfig.OR >> m_playerConfig.OG >> m_playerConfig.OB >>
 			m_playerConfig.OT >> m_playerConfig.V >> m_playerConfig.S;
 
 		// Read enemy config
-		file >> m_enemyConfig.SR >> m_enemyConfig.CR >> 
-			m_enemyConfig.OR >> m_enemyConfig.OG >> m_enemyConfig.OB >> 
+		file >> m_enemyConfig.SR >> m_enemyConfig.CR >>
+			m_enemyConfig.OR >> m_enemyConfig.OG >> m_enemyConfig.OB >>
 			m_enemyConfig.OT >> m_enemyConfig.VMIN >> m_enemyConfig.VMAX >>
 			m_enemyConfig.L >> m_enemyConfig.SI >> m_enemyConfig.SMIN >> m_enemyConfig.SMAX;
 
 		// Read bullet config
-		file >> m_bulletConfig.SR >> m_bulletConfig.CR >> 
-			m_bulletConfig.FR >> m_bulletConfig.FG >> m_bulletConfig.FB >> 
-			m_bulletConfig.OR >> m_bulletConfig.OG >> m_bulletConfig.OB >> 
+		file >> m_bulletConfig.SR >> m_bulletConfig.CR >>
+			m_bulletConfig.FR >> m_bulletConfig.FG >> m_bulletConfig.FB >>
+			m_bulletConfig.OR >> m_bulletConfig.OG >> m_bulletConfig.OB >>
 			m_bulletConfig.OT >> m_bulletConfig.V >> m_bulletConfig.S;
 
 		file.close(); // close file
 
-		// Setup ImGui binding
-		ImGui::SFML::Init(m_window);
+		// Setup ImGui binding (ignore return value to silence warnings)
+		(void)ImGui::SFML::Init(m_window);
 		ImGui::GetStyle().ScaleAllSizes(2.0f);
 		ImGui::GetIO().FontGlobalScale = 2.0f;
 
 		// Setup Enemy Spawn Area
-		m_enemySpawnArea.minX = m_enemyConfig.SR * 1.5;
-		m_enemySpawnArea.minY = m_enemyConfig.SR * 1.5;
-		m_enemySpawnArea.maxX = m_screenSize.x - m_enemySpawnArea.minX;
-		m_enemySpawnArea.maxY = m_screenSize.y - m_enemySpawnArea.minY;
+		m_enemySpawnArea.minX = static_cast<short>(m_enemyConfig.SR * 1.5);
+		m_enemySpawnArea.minY = static_cast<short>(m_enemyConfig.SR * 1.5);
+		m_enemySpawnArea.maxX = static_cast<short>(m_screenSize.x - m_enemySpawnArea.minX);
+		m_enemySpawnArea.maxY = static_cast<short>(m_screenSize.y - m_enemySpawnArea.minY);
 
 		spawnPlayer();
 
 		return 0;
 	}
 
-	void update() {
+	void update()
+	{
 		// Destroy entities
 		auto view = m_registry.view<TPendingDestruction>();
 		for (auto entity : view)
@@ -196,7 +208,7 @@ class Game {
 			// Update enemy position
 			transform.position += transform.velocity;
 
-			float radius = m_enemyConfig.SR * 1.5;
+			float radius = static_cast<float>(m_enemyConfig.SR * 1.5);
 
 			// Bounce off left/right walls
 			if (transform.position.x - radius < 0)
@@ -227,29 +239,25 @@ class Game {
 		auto player = m_registry.view<TPlayer>().front();
 		auto& playerTransform = m_registry.get<CTransform>(player);
 		auto& playerInput = m_registry.get<CInput>(player);
-		
-		//playerTransform.position.x += playerTransform.velocity.x * (playerInput.right - playerInput.left);
-		
+
 		if (playerInput.left) playerTransform.position.x -= playerTransform.velocity.x;
 		if (playerInput.right) playerTransform.position.x += playerTransform.velocity.x;
-
 		if (playerInput.up) playerTransform.position.y -= playerTransform.velocity.y;
 		if (playerInput.down) playerTransform.position.y += playerTransform.velocity.y;
 
-		int playerBound = m_playerConfig.SR * 1.5;
+		int playerBound = static_cast<int>(m_playerConfig.SR * 1.5);
 
 		// Clamp player position so it stays within the screen
 		if (playerTransform.position.x < playerBound)
-			playerTransform.position.x = playerBound;
+			playerTransform.position.x = static_cast<float>(playerBound);
 		else if (playerTransform.position.x > m_screenSize.x - playerBound)
-			playerTransform.position.x = m_screenSize.x - playerBound;
+			playerTransform.position.x = m_screenSize.x - static_cast<float>(playerBound);
 
 		if (playerTransform.position.y < playerBound)
-			playerTransform.position.y = playerBound;
+			playerTransform.position.y = static_cast<float>(playerBound);
 		else if (playerTransform.position.y > m_screenSize.y - playerBound)
-			playerTransform.position.y = m_screenSize.y - playerBound;
+			playerTransform.position.y = m_screenSize.y - static_cast<float>(playerBound);
 	}
-
 
 	void sUserInput()
 	{
@@ -280,7 +288,8 @@ class Game {
 				case sf::Keyboard::Scancode::Escape:
 					m_running = false;
 					break;
-				default: break;
+				default:
+					break;
 				}
 			}
 
@@ -300,7 +309,8 @@ class Game {
 				case sf::Keyboard::Scancode::D:
 					m_registry.get<CInput>(m_registry.view<TPlayer>().front()).right = 0;
 					break;
-				default: break;
+				default:
+					break;
 				}
 			}
 		}
@@ -317,26 +327,36 @@ class Game {
 		float sumRadii;
 
 		// Iterate over each pair of entities (using structured bindings)
-		for (auto [entity1, transform1, col1] : view.each()) {
-			for (auto [entity2, transform2, col2] : view.each()) {
+		for (auto [entity1, transform1, col1] : view.each())
+		{
+			for (auto [entity2, transform2, col2] : view.each())
+			{
 				// Avoid checking a pair twice
-				if (entity1 < entity2) {
+				if (entity1 < entity2)
+				{
 					// Use squared distance for efficiency
 					sf::Vector2f diff = transform1.position - transform2.position;
 					distSquared = diff.x * diff.x + diff.y * diff.y;
 					sumRadii = col1.radius + col2.radius;
-					if (distSquared < sumRadii * sumRadii) {
+					if (distSquared < sumRadii * sumRadii)
+					{
 						// If one entity is a player and the other an enemy or small enemy, mark both for destruction, and respawn the player
-						if (m_registry.all_of<TPlayer>(entity1) && (m_registry.all_of<TEnemy>(entity2) || m_registry.all_of<TSmallEnemy>(entity2))
-							|| m_registry.all_of<TPlayer>(entity2) && (m_registry.all_of<TEnemy>(entity1) || m_registry.all_of<TSmallEnemy>(entity1))) {
+						if (m_registry.all_of<TPlayer>(entity1) &&
+							(m_registry.all_of<TEnemy>(entity2) || m_registry.all_of<TSmallEnemy>(entity2))
+							|| m_registry.all_of<TPlayer>(entity2) &&
+							(m_registry.all_of<TEnemy>(entity1) || m_registry.all_of<TSmallEnemy>(entity1)))
+						{
 							m_registry.emplace<TPendingDestruction>(entity1);
 							m_registry.emplace<TPendingDestruction>(entity2);
 							m_score -= 1000;
 							spawnPlayer();
 						}
 						// If one entity is a bullet and the other an enemy or small enemy, mark both for destruction
-						else if (m_registry.all_of<TBullet>(entity1) && (m_registry.all_of<TEnemy>(entity2) || m_registry.all_of<TSmallEnemy>(entity2))
-							|| m_registry.all_of<TBullet>(entity2) && (m_registry.all_of<TEnemy>(entity1) || m_registry.all_of<TSmallEnemy>(entity1))) {
+						else if (m_registry.all_of<TBullet>(entity1) &&
+							(m_registry.all_of<TEnemy>(entity2) || m_registry.all_of<TSmallEnemy>(entity2))
+							|| m_registry.all_of<TBullet>(entity2) &&
+							(m_registry.all_of<TEnemy>(entity1) || m_registry.all_of<TSmallEnemy>(entity1)))
+						{
 							m_registry.emplace<TPendingDestruction>(entity1);
 							m_registry.emplace<TPendingDestruction>(entity2);
 							m_score += 100;
@@ -360,23 +380,25 @@ class Game {
 
 		m_registry.emplace<CTransform>(player,
 			Vector2f(m_screenSize.x / 2.0f, m_screenSize.y / 2.0f), // Position
-			Vector2f(m_playerConfig.S, m_playerConfig.S), // Velocity
-			1.0f // Angle
+			Vector2f(m_playerConfig.S, m_playerConfig.S),             // Velocity
+			1.0f                                                        // Angle
 		);
 
 		m_registry.emplace<CShape>(player,
-			m_playerConfig.SR,	// Shape Radius
-			m_playerConfig.V,	// Verticies
-			sf::Color(m_playerConfig.FR, m_playerConfig.FG, m_playerConfig.FB), // Fill Color
-			sf::Color(m_playerConfig.OR, m_playerConfig.OG, m_playerConfig.OB), // Outline Color
-			m_playerConfig.OT	// Outline Thickness
+			m_playerConfig.SR,                                  // Shape Radius
+			m_playerConfig.V,                                   // Verticies
+			sf::Color(static_cast<uint8_t>(m_playerConfig.FR),
+				static_cast<uint8_t>(m_playerConfig.FG),
+				static_cast<uint8_t>(m_playerConfig.FB)), // Fill Color
+			sf::Color(static_cast<uint8_t>(m_playerConfig.OR),
+				static_cast<uint8_t>(m_playerConfig.OG),
+				static_cast<uint8_t>(m_playerConfig.OB)), // Outline Color
+			m_playerConfig.OT                                  // Outline Thickness
 		);
 
-		m_registry.emplace<CColission>(player, m_playerConfig.CR + m_playerConfig.OT);
-
+		m_registry.emplace<CColission>(player, static_cast<float>(m_playerConfig.CR + m_playerConfig.OT));
 		m_registry.emplace<CInput>(player);
 		m_registry.emplace<TPlayer>(player);
-
 	}
 
 	void spawnEnemy()
@@ -386,25 +408,32 @@ class Game {
 
 		m_registry.emplace<CTransform>(enemy,
 			// Position
-			Vector2f((rand() % (1 + m_enemySpawnArea.maxX - m_enemySpawnArea.minX)) + m_enemySpawnArea.minX,
-				(rand() % (1 + m_enemySpawnArea.maxY - m_enemySpawnArea.minY)) + m_enemySpawnArea.minY),
+			Vector2f(
+				static_cast<float>((rand() % (1 + m_enemySpawnArea.maxX - m_enemySpawnArea.minX)) + m_enemySpawnArea.minX),
+				static_cast<float>((rand() % (1 + m_enemySpawnArea.maxY - m_enemySpawnArea.minY)) + m_enemySpawnArea.minY)
+			),
 			// Velocity
-			Vector2f((static_cast<float> (rand()) / static_cast<float>(RAND_MAX)) * (m_enemyConfig.SMAX - m_enemyConfig.SMIN),
-				(static_cast<float> (rand()) / static_cast<float>(RAND_MAX)) * (m_enemyConfig.SMAX - m_enemyConfig.SMIN)),
+			Vector2f(
+				(static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * (m_enemyConfig.SMAX - m_enemyConfig.SMIN),
+				(static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * (m_enemyConfig.SMAX - m_enemyConfig.SMIN)
+			),
 			// Angle
-			static_cast<float> (rand()) / 360 // Angle
+			static_cast<float>(rand()) / 360
 		);
 
 		m_registry.emplace<CShape>(enemy,
-			m_enemyConfig.SR,	// Shape Radius
+			m_enemyConfig.SR,                                           // Shape Radius
 			(rand() % (1 + m_enemyConfig.VMIN - m_enemyConfig.VMAX)) + m_enemyConfig.VMIN, // Verticies
-			sf::Color(rand() % 255, rand() % 255, rand() % 255), // Fill Color
-			sf::Color(m_enemyConfig.OR, m_enemyConfig.OG, m_enemyConfig.OB), // Outline Color
-			m_enemyConfig.OT	// Outline Thickness
+			sf::Color(static_cast<uint8_t>(rand() % 255),
+				static_cast<uint8_t>(rand() % 255),
+				static_cast<uint8_t>(rand() % 255)),             // Fill Color
+			sf::Color(static_cast<uint8_t>(m_enemyConfig.OR),
+				static_cast<uint8_t>(m_enemyConfig.OG),
+				static_cast<uint8_t>(m_enemyConfig.OB)),         // Outline Color
+			m_enemyConfig.OT                                            // Outline Thickness
 		);
 
-		m_registry.emplace<CColission>(enemy, m_enemyConfig.CR + m_enemyConfig.OT);
-
+		m_registry.emplace<CColission>(enemy, static_cast<float>(m_enemyConfig.CR + m_enemyConfig.OT));
 		m_registry.emplace<TEnemy>(enemy);
 
 		m_lastEnemySpawnTime = m_currentFrame;
@@ -418,7 +447,8 @@ class Game {
 	{
 		ImGui::Begin("AstralReign");
 		ImGui::Text("Score: %d", m_score);
-		if (ImGui::Button("Exit")) {
+		if (ImGui::Button("Exit"))
+		{
 			m_running = false;
 		}
 		ImGui::End();
@@ -428,7 +458,8 @@ class Game {
 	{
 		m_window.clear();
 		auto view = m_registry.view<CTransform, CShape>();
-		for (auto [entity, transform, shape] : view.each()) {
+		for (auto [entity, transform, shape] : view.each())
+		{
 			shape.circle.setPosition(transform.position);
 			shape.circle.setRotation(sf::degrees(++transform.angle));
 			m_window.draw(shape.circle);
@@ -437,19 +468,17 @@ class Game {
 		m_window.display();
 	}
 
-	
-
 public:
 	Game(const std::string& config)
 	{
 		init(config);
 	}
 
-	void run() {
-
+	void run()
+	{
 		// TODO pause function
-
-		while (m_running) {
+		while (m_running)
+		{
 			update();
 
 			ImGui::SFML::Update(m_window, m_deltaClock.restart());
@@ -462,7 +491,7 @@ public:
 
 			sGUI();
 			sRender();
-			
+
 			m_score += 3;
 			m_currentFrame++;
 		}
